e1455f79c18f909d698633c56f5c9c94e747118b
dako <chididi_gloria@hotmail.com>
2019-05-13 17:04:39 +1000

    Update u-net-p2m.py

--- models/u-net-p2m.py
+++ models/u-net-p2m.py
@@ -48,7 +48,8 @@ def read_images(filepath):
     images_A = []
     images_B = []
     for i in range(len(path)):
-        image = nib.load(path[0])
+        print(path[i])
+        image = nib.load(path[i])
         image = image.get_data()
         _,_,w = image.shape
         image_A = image[0,:,:int(w/2)]
@@ -293,7 +294,7 @@ curr_img = np.reshape(valid_ground[0], (256,256))
 plt.imshow(curr_img, cmap='gray')
 '''
 #%%
-
+'''
 batch_size = 1
 epochs = 200
 inChannel = 1
@@ -312,6 +313,7 @@ autoencoder_train = autoencoder.fit(train_X, train_ground, batch_size=batch_size
 #loss = autoencoder_train.history['loss']
 #val_loss = autoencoder_train.history['val_loss']
 autoencoder.save('u-net-p2m.h5')
+'''
 '''
 #%%
 filepath_test_X = sys.argv[3]#'../Documents/MRI_data/dataset/X/*'#sys.argv[3]
