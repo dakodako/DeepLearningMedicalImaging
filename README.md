25d5d1981a750e6943b702716038af0e7765cffc
dako <chididi_gloria@hotmail.com>
2019-05-14 15:25:43 +1000

    add data

---
+++ README.md
@@ -0,0 +1,8 @@
+### There different models are going to be used.
+
+1. U-net
+   
+    u-net-p2m.py is the Keras implementation of the U-net model.
+
+2. pix2pix
+3. Cycle-GAN (to be implemented)
---
+++ models/evaluate_u-net.py
@@ -0,0 +1,50 @@
+#%%
+
+from keras import models
+from keras.models import load_model
+import numpy as np 
+from skimage.transform import rotate, resize
+import nibabel as nib 
+from glob import glob 
+import sys 
+import matplotlib.pyplot as plt
+
+#%%
+batch_size = 1
+path = glob('/Users/chid/Documents/DeepLearning/datasets/p2m/val/*')
+print(path)
+#%%
+batch_images = np.random.choice(path, size = batch_size)
+print(batch_images)
+#%%
+a = nib.load(batch_images[0])
+a = a.get_fdata()
+print(a.shape)
+#%%
+target = a[0,:,0:255]
+X = a[:,:,255:511]
+mi = np.min(X)
+m = np.max(X)
+X = (X - mi)/(m - mi)
+mi = np.min(target)
+m = np.max(target)
+target = (target - mi)/(m - mi)
+plt.imshow(np.squeeze(X), cmap= 'gray')
+X = np.expand_dims(X, 3)
+print(X.shape)
+
+#%%
+model = load_model('/Users/chid/Documents/DeepLearning/models/u-net-p2m_l2.h5')
+model.summary()
+
+#%%
+
+pred = model.predict(X)
+#%%
+print(pred.shape)
+plt.imshow(np.squeeze(pred), cmap = 'gray')
+#%%
+plt.imshow(np.squeeze(target), cmap = 'gray')
+#%%
+loss = np.mean(np.square(pred- target))
+print(loss)
